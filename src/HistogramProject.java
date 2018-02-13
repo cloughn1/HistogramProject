a188c45697b98cd6a624d7b0bfe47bfc5e806b13
Patrick Sullivan <bpsullivan3@gmail.com>
2018-02-13 17:23:57 -0500

    Able to read in data file

--- src/HistogramProject.java
+++ src/HistogramProject.java
@@ -1,6 +1,15 @@
+import java.io.*;
 
 public class HistogramProject {
 	public static void main(String[] args) {
-		
+		File dataFile = new File("src/data.txt");
+
+		try {
+			FileReader fileReader = new FileReader(dataFile.getAbsoluteFile());
+			BufferedReader reader = new BufferedReader(fileReader);
+
+		} catch(FileNotFoundException e){
+			System.out.println("Data file not found");
+		}
 	}
 }
